aacf293d94a54ca2e9627d7a8b9f9c3eaf8f825c
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2023-08-24 00:51:22 +0300

    adding neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	"folke/tokyonight.nvim"
+})
+
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.cmd([[colorscheme tokyonight]])
