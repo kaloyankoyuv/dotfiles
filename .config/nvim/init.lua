554428c23e99cdc102323f6e83c733abff53fc84
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2023-08-24 20:07:49 +0300

    removing neovim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-	"folke/tokyonight.nvim"
-})
-
-
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.cursorline = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.scrolloff = 5
-
-vim.cmd([[colorscheme tokyonight]])
-vim.cmd([[nnoremap <leader>f :e `find -type f \\| dmenu -l 16`<cr>]])
