55cdba3a4cc6caf3d60843146e34e1094e592d36
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2023-09-13 13:10:39 +0300

    moving the autostart progs in rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -567,3 +567,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+awful.spawn("setxkbmap -layout 'us,bg(phonetic)' -option grp:toggle -option ctrl:swapcaps")
+awful.spawn("emacs --daemon")
